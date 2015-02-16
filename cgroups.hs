bec4fd21fab40185ecb90a46a14a502712f8e924
Brandon Crisp <brandon.m.crisp@gmail.com>
2015-02-16 11:48:26 -0800

    Refactoring cgroups code

--- cgroups.hs
+++ cgroups.hs
@@ -7,8 +7,11 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad
 import Control.Monad.Reader (lift)
-import Webapp
 import Data.List.Split as L
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
 
 import Data.Monoid (mconcat)
 
@@ -18,14 +21,23 @@ cgroupDirectory = "/sys/fs/cgroup"
 subsystems = ["blkio", "cpu", "cpuacct", "cpuset", "devices", "freezer", "memory", "net_cls", "net_prio", "ns"]
 
 app req respond = do
-	let l = BS.unpack $ rawPathInfo req 
+	let l = (BS.unpack . rawPathInfo) req 
+	let m = (BS.unpack . requestMethod) req
 	liftIO $ putStrLn l
-	lines <- liftIO $ readFile $ mconcat [cgroupDirectory, l, "/tasks"]
-	let splitLines = splitOn "\n" lines
-	liftIO $ print splitLines
-	respond $ index  splitLines
+	liftIO $ putStrLn $ case m of
+		"GET" -> "User requested with GET" $ do 
+			lines <- liftIO $ readFile $ mconcat [cgroupDirectory, l, "/tasks"]
+			let splitLines = splitOn "\n" lines
+			liftIO $ print splitLines
+			respond $ index splitLines
 
 main = do
 	let port = 3000
 	putStrLn $ "Listening on " ++ show port
-	begin port app
+	run port app
+
+
+--begin port a = run port a
+
+
+index x = responseLBS status200 [(hContentType, "application/json")] (encode x) 
